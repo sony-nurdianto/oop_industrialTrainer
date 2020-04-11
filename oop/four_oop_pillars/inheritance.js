7f951d723e093da867b44b4eb18e44ff2fb3fe56
sony-nurdianto <sonynurdianto445@gmail.com>
2020-04-11 12:33:24 +0700

    add new method

--- oop/four_oop_pillars/inheritance.js
+++ oop/four_oop_pillars/inheritance.js
@@ -1,23 +1,56 @@
+// class People {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+// }
+
+// class Person extends People {
+//   constructor(name, age, job, status) {
+//     super(name, age); // this is how to get the inherit properties/methods
+//     this.job = job;
+//     var status = status;
+//     this.showStatus = function () {
+//       return status;
+//     };
+//   }
+// }
+
+// const personOld = new People("Bill", 15);
+// console.log(personOld);
+
+// const person1 = new Person("Ani", 15, "Software Engineer", "married");
+// console.log(person1);
+
 class People {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
+  constructor( obj = {}) {
+    this.name = obj.name;
+    this.age = obj.age;
   }
 }
 
 class Person extends People {
-  constructor(name, age, job, status) {
-    super(name, age); // this is how to get the inherit properties/methods
-    this.job = job;
-    var status = status;
+  constructor(obj = {}) {
+    super(obj); // this is how to get the inherit properties/methods
+    this.job = obj.job;
+    const status = obj.status;
     this.showStatus = function () {
       return status;
     };
   }
 }
 
-const personOld = new People("Bill", 15);
-console.log(personOld);
+const person = new Person ({
+  name : "anonymus",
+  age : "anonymus",
+  job : "anonymus",
+  status : "anonymus"
+})
 
-const person1 = new Person("Ani", 15, "Software Engineer", "married");
-console.log(person1);
+console.log(person);
+person.showStatus();
+// const personOld = new People("Bill", 15);
+// console.log(personOld);
+
+// const person1 = new Person("Ani", 15, "Software Engineer", "married");
+// console.log(person1);
